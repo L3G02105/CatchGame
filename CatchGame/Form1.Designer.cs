00128d1049bf010a47ab4b8b98d50331f866ed02
L3G0 <L3G0@DESKTOP-J97OU98>
2025-04-16 15:09:06 +0300

    Добавьте файлы проекта.

---
+++ CatchGame/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace CatchGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gamePanel = new System.Windows.Forms.Panel();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // gamePanel
+            // 
+            this.gamePanel.BackColor = System.Drawing.Color.LightGray;
+            this.gamePanel.Location = new System.Drawing.Point(556, 58);
+            this.gamePanel.Name = "gamePanel";
+            this.gamePanel.Size = new System.Drawing.Size(400, 400);
+            this.gamePanel.TabIndex = 0;
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(312, 90);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(58, 13);
+            this.lblTime.TabIndex = 1;
+            this.lblTime.Text = "Время: 60";
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(317, 195);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(42, 13);
+            this.lblScore.TabIndex = 2;
+            this.lblScore.Text = "Счёт: 0";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(284, 350);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(98, 36);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Начать игру\r\n";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click_1);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1044, 610);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.gamePanel);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel gamePanel;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ CatchGame/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CatchGame
+{
+    public partial class Form1 : Form
+    {
+
+        private Timer gameTimer = new Timer();
+        private Timer spawnTimer = new Timer();
+        private Timer countdownTimer = new Timer();
+        private Random rand = new Random();
+
+        Rectangle player = new Rectangle(190, 190, 20, 20);
+        List<Rectangle> objects = new List<Rectangle>();
+
+        int score = 0;
+        int timeLeft = 60;
+        int spawnInterval = 1000;
+
+        bool moveUp, moveDown, moveLeft, moveRight;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.KeyPreview = true;
+
+            gameTimer.Interval = 20;
+            gameTimer.Tick += GameTick;
+
+            spawnTimer.Interval = spawnInterval;
+            spawnTimer.Tick += SpawnObject;
+
+            countdownTimer.Interval = 1000;
+            countdownTimer.Tick += UpdateTimer;
+
+            gamePanel.Paint += GamePanel_Paint;
+            this.KeyDown += Form1_KeyDown;
+            this.KeyUp += Form1_KeyUp;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            score = 0;
+            timeLeft = 60;
+            spawnInterval = 1000;
+            player.X = 190;
+            player.Y = 190;
+            objects.Clear();
+
+            lblScore.Text = "Счёт: 0";
+            lblTime.Text = "Время: 60";
+
+            gameTimer.Start();
+            spawnTimer.Start();
+            countdownTimer.Start();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up) moveUp = true;
+            if (e.KeyCode == Keys.Down) moveDown = true;
+            if (e.KeyCode == Keys.Left) moveLeft = true;
+            if (e.KeyCode == Keys.Right) moveRight = true;
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up) moveUp = false;
+            if (e.KeyCode == Keys.Down) moveDown = false;
+            if (e.KeyCode == Keys.Left) moveLeft = false;
+            if (e.KeyCode == Keys.Right) moveRight = false;
+        }
+
+        private void GameTick(object sender, EventArgs e)
+        {
+            int speed = 5;
+
+            if (moveUp && player.Y > 0) player.Y -= speed;
+            if (moveDown && player.Y < gamePanel.Height - player.Height) player.Y += speed;
+            if (moveLeft && player.X > 0) player.X -= speed;
+            if (moveRight && player.X < gamePanel.Width - player.Width) player.X += speed;
+
+            for (int i = objects.Count - 1; i >= 0; i--)
+            {
+                if (player.IntersectsWith(objects[i]))
+                {
+                    objects.RemoveAt(i);
+                    score++;
+                    lblScore.Text = $"Счёт: {score}";
+                }
+            }
+
+            gamePanel.Invalidate();
+        }
+
+        private void btnStart_Click_1(object sender, EventArgs e)
+        {
+            btnStart.Click += btnStart_Click;
+        }
+
+        private void SpawnObject(object sender, EventArgs e)
+        {
+            int x = rand.Next(gamePanel.Width - 15);
+            int y = rand.Next(gamePanel.Height - 15);
+            objects.Add(new Rectangle(x, y, 15, 15));
+        }
+
+        private void UpdateTimer(object sender, EventArgs e)
+        {
+            timeLeft--;
+            lblTime.Text = $"Время: {timeLeft}";
+
+            if (timeLeft <= 0)
+            {
+                gameTimer.Stop();
+                spawnTimer.Stop();
+                countdownTimer.Stop();
+                MessageBox.Show($"Игра окончена! Ваш счёт: {score}");
+            }
+            else if (spawnInterval > 200)
+            {
+                spawnInterval -= 50;
+                spawnTimer.Interval = spawnInterval;
+            }
+        }
+
+        private void GamePanel_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(Brushes.Blue, player);
+            foreach (var obj in objects)
+            {
+                e.Graphics.FillEllipse(Brushes.Red, obj);
+            }
+        }
+    }
+}
